47ba9e7e1279b8c72b19934673c40c3353f0ade3
Mariana Montes <mariana.montes@kuleuven.be>
2021-07-20 18:59:20 +0200

    add license text to js files

--- src/fillIndex.js
+++ src/fillIndex.js
@@ -1,3 +1,19 @@
+/* Interactive visualization of token-level distributional models
+Copyright (C) 2021  Thomas Wielfaert and Mariana Montes - QLVL, KU Lueven
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 function displayLemmas(data) {
     const col1 = d3.select('#lemmas')
         .append("div").attr("class", "col")
