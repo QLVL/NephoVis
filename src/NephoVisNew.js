116cd68396f7ebd787a76d3a4fc635da951380b7
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-04 12:15:28 +0100

    Add requested files mapping to NephoVis parent class

--- src/NephoVisNew.js
+++ src/NephoVisNew.js
@@ -2,9 +2,10 @@ class NephoVis {
 	constructor(level, type) {
 		this.level = level;
 		this.type = type;
+		this.requestedFiles = RequestedFilesMapper[level];
 
 		// Define the data loader and load the CSV files for this type
-		this.dataLoader = new DataLoader(this.type);
+		this.dataLoader = new DataLoader(this.type, this.requestedFiles);
 		this.dataLoader.loadData();
 	}
 }
