3210dc57e66c401ada009890a89bedea5dfb55a1
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-15 09:58:30 +0100

    Specify level 3 requested files

--- src/RequestedFilesMapper.js
+++ src/RequestedFilesMapper.js
@@ -1,5 +1,6 @@
 const RequestedFilesMapper = {
 	1: ["models", "medoids"],
-	model: ["models", "medoids"]
+	model: ["models", "medoids"],
+	token: ["tokens", "variables", "focdists"]
 	// TODO add other levels files and other things
 };
