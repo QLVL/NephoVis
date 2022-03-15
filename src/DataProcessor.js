aaa3529d201798e383bd585d8a302789482082bf
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-15 16:28:50 +0100

    Introduce variability to DataProcessor class

--- src/DataProcessor.js
+++ src/DataProcessor.js
@@ -1,6 +1,7 @@
 class DataProcessor {
-	constructor(datasets) {
+	constructor(datasets, centralDataset) {
 		this.datasets = datasets;
+		this.centralDataset = centralDataset;
 
 		this.extractColumnNames();
 		this.extractOrderFeatures();
@@ -8,7 +9,7 @@ class DataProcessor {
 
 	extractColumnNames() {
 		// We infer the column names from the keys of first entry of the "models" data point
-		this.columnNames = Object.keys(this.datasets["models"][0]);
+		this.columnNames = Object.keys(this.datasets[this.centralDataset][0]);
 
 		// Now, we need to filter the following columns:
 		// 1: columns ending with .x (these are model x coordinates)
@@ -16,7 +17,7 @@ class DataProcessor {
 		// 3: columns which have no unique values
 		this.variableNames = this.columnNames.filter((columnName) => {
 			return (!([ ".x", ".y" ].includes(columnName.slice(-2))) // 1 and 2 
-					&& Helpers.uniqueValues(this.datasets["models"], columnName).length > 1 // 3
+					&& Helpers.uniqueValues(this.datasets[this.centralDataset], columnName).length > 1 // 3
 				);
 		});
 
@@ -25,7 +26,7 @@ class DataProcessor {
 		// 2: models with NaN values
 		this.nominalNames = this.variableNames.filter((columnName) => {
 			return (!columnName.startsWith("_") // 1
-            	&& !Helpers.uniqueValues(this.datasets["models"], columnName).every(value => 
+            	&& !Helpers.uniqueValues(this.datasets[this.centralDataset], columnName).every(value => 
             			{ return (!isNaN(value)); })); //2
 		});
 
@@ -34,11 +35,11 @@ class DataProcessor {
 		// Second pass: filter more columns
 		// filter all columns with more than 10 unique values
 		this.nominalNames = this.nominalNames.filter((columnName) => { 
-			return Helpers.uniqueValues(this.datasets["models"], columnName).length <= 10 });
+			return Helpers.uniqueValues(this.datasets[this.centralDataset], columnName).length <= 10 });
 		this.nominalNames.push("Reset"); // I don't know why this needs to be in
 
 		this.numeralNames = this.variableNames.filter((columnName) => {
-			return Helpers.uniqueValues(this.datasets["models"], columnName).every(value => !isNaN(value));
+			return Helpers.uniqueValues(this.datasets[this.centralDataset], columnName).every(value => !isNaN(value));
 		});
 		this.numeralNames.push("Reset") // I don't know why this needs to be in
 
