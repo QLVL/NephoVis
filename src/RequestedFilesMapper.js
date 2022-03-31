74d5cbc4774e274f4c58361384999806e450ef2c
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-31 11:49:33 +0200

    We need to load ppmi in token level as wel to know if frequency table is available

--- src/RequestedFilesMapper.js
+++ src/RequestedFilesMapper.js
@@ -2,7 +2,7 @@ const RequestedFilesMapper = {
 	1: ["models", "medoids"],
 	model: ["models", "medoids"],
 	aggregate: ["models", "modelsdist", "tokens", "variables"],
-	token: ["tokens", "variables", "focdists"],
+	token: ["tokens", "variables", "focdists", "ppmi"],
 	frequency: ['variables', 'ppmi'],
 	cws: ['variables'],
 	distance: ["modelsdist"]
