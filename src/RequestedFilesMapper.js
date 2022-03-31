ba4e29deecb3b679989a1a4ee485616a1b6ac9fa
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-31 12:04:22 +0200

    PPMI is not needed to disable frequency in level 3 after all

--- src/RequestedFilesMapper.js
+++ src/RequestedFilesMapper.js
@@ -2,7 +2,7 @@ const RequestedFilesMapper = {
 	1: ["models", "medoids"],
 	model: ["models", "medoids"],
 	aggregate: ["models", "modelsdist", "tokens", "variables"],
-	token: ["tokens", "variables", "focdists", "ppmi"],
+	token: ["tokens", "variables", "focdists"],
 	frequency: ['variables', 'ppmi'],
 	cws: ['variables'],
 	distance: ["modelsdist"]
